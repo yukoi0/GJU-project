9e199b72f289ac7cd72c1ad76d0e41da803f46ab
Mnourai <nouraialslman@gmail.com>
2024-03-08 17:02:22 +0300

    button update

--- GJU trashpickup/Assets/TextMesh Pro/Examples & Extras/Scripts/EnvMapAnimator.cs
+++ GJU trashpickup/Assets/TextMesh Pro/Examples & Extras/Scripts/EnvMapAnimator.cs
@@ -1,3 +1,6 @@
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
 ﻿using UnityEngine;
 using System.Collections;
 using TMPro;
@@ -33,3 +36,44 @@ public class EnvMapAnimator : MonoBehaviour {
         }
 	}
 }
+=======
+>>>>>>> Stashed changes
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class EnvMapAnimator : MonoBehaviour {
+
+    //private Vector3 TranslationSpeeds;
+    public Vector3 RotationSpeeds;
+    private TMP_Text m_textMeshPro;
+    private Material m_material;
+    
+
+    void Awake()
+    {
+        //Debug.Log("Awake() on Script called.");
+        m_textMeshPro = GetComponent<TMP_Text>();
+        m_material = m_textMeshPro.fontSharedMaterial;
+    }
+
+    // Use this for initialization
+	IEnumerator Start ()
+    {
+        Matrix4x4 matrix = new Matrix4x4(); 
+        
+        while (true)
+        {
+            //matrix.SetTRS(new Vector3 (Time.time * TranslationSpeeds.x, Time.time * TranslationSpeeds.y, Time.time * TranslationSpeeds.z), Quaternion.Euler(Time.time * RotationSpeeds.x, Time.time * RotationSpeeds.y , Time.time * RotationSpeeds.z), Vector3.one);
+             matrix.SetTRS(Vector3.zero, Quaternion.Euler(Time.time * RotationSpeeds.x, Time.time * RotationSpeeds.y , Time.time * RotationSpeeds.z), Vector3.one);
+
+            m_material.SetMatrix("_EnvMatrix", matrix);
+
+            yield return null;
+        }
+	}
+}
+<<<<<<< Updated upstream
+=======
+>>>>>>> a0286d8082c69f95fb64ae93dbf7b3b8a4f90d84
+>>>>>>> Stashed changes
